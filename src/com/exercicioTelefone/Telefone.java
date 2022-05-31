961053f8123b2ae79604f7930f8e20b503406264
karolina <karolinabraccialli@gmail.com>
2022-05-31 18:08:46 -0300

    Criando o repositorio

--- src/com/exercicioTelefone/Telefone.java
+++ src/com/exercicioTelefone/Telefone.java
@@ -4,46 +4,65 @@ import java.util.Scanner;
 
 public class Telefone {
     public static void main(String[] args) {
-//        Scanner scanner=new Scanner(System.in);
-//        System.out.println("Escreva uma frase: ");
-//        String frase = scanner.next();
-//        int resultado;
-//
-//        char[] chars = frase.toCharArray();
-//        for (char el: chars) {
-//            switch (el){
-//                case 'A','B','C'
-//                    resultado += 2;
-//                    break;
-//                    case 'D', 'E', 'F'
-//                        resultado += 3;
-//                    break;
-//                case 'G', 'H', 'I'
-//                    resultado += 4;
-//                    break;
-//                case 'J', 'K', 'L'
-//                    resultado += 5;
-//                    break;
-//                case 'M', 'N', 'O'
-//                    resultado += 6;
-//                    break;
-//                case 'P', 'Q', 'R', 'S'
-//                    resultado += 7;
-//                    break;
-//                case 'T', 'U', 'V'
-//                    resultado += 8;
-//                    break;
-//                case 'W', 'X', 'Y', 'Z'
-//                    resultado += 9;
-//                    break;
-//
-//                default:
-//                    throw new IllegalStateException("Unexpected value: " + el);
-//            }
-//
-//        }
-//
-//        System.out.println(resultado);
-//    }
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Escreva uma frase: ");
+        String frase = scanner.next();
+        String resultado = "";
+
+        char[] chars = frase.toCharArray();
+        for (char el: chars) {
+            switch (el){
+                case 'A':
+                case 'B':
+                    case 'C':
+                    resultado += "2";
+                    break;
+                case 'D':
+                case 'E':
+                   case 'F':
+                        resultado += "3";
+                    break;
+                case 'G':
+                case 'H':
+                    case 'I':
+                    resultado += "4";
+                    break;
+                case 'J':
+                case 'K':
+                    case'L':
+                    resultado += "5";
+                    break;
+                case 'M':
+                case 'N':
+                    case 'O':
+                    resultado += "6";
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                    case 'S':
+                    resultado += "7";
+                    break;
+                case 'T':
+                case 'U':
+                    case 'V':
+                    resultado += "8";
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                    case 'Z':
+                    resultado += "9";
+                    break;
+                case '1':
+                case '-':
+                case '0':
+                    resultado += el;
+                    break;
+            }
+
+        }
+
+        System.out.println(resultado);
     }
 }
